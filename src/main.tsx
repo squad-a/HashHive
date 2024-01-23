672596229acd34cb46b63d2f3e4e1b55cae3ccd5
Ankit Bisen <ankitbisen288@gmail.com>
2024-01-23 10:28:19 +0530

    integration/material-ui

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { useAppStore } from './utils/store.tsx';
 import { ProtectedRoute } from './shared/ProtectedRoute.tsx';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 const Auth = lazy(() => import('./pages/Auth/Index'));
 const Home = lazy(() => import('./pages/Home/Index'));
@@ -34,10 +36,12 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <Suspense fallback={<>Loading ...</>}>
-        <RouterProvider router={router} />
-      </Suspense>
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+      <ApolloProvider client={client}>
+        <Suspense fallback={<>Loading ...</>}>
+          <RouterProvider router={router} />
+        </Suspense>
+      </ApolloProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/theme.ts
@@ -0,0 +1,34 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Poppins', 'sans-serif'].join(','),
+    h1: {
+      fontSize: '4rem',
+      fontWeight: '600'
+    }
+  },
+  palette: {
+    primary: {
+      main: '#212121'
+    },
+    secondary: {
+      main: '#fff'
+    },
+    info: {
+      main: '#ffc200'
+    }
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920
+    }
+  }
+  // ... other theme options ...
+});
+
+export default theme;
