d7c755b612e104c6216b4d88ccb4c6862c2a99da
elamandeep <deep.aman6174@gmail.com>
2024-01-24 11:06:11 +0530

    fix: Resolve TypeScript code generation type issue

--- src/__generated__/gql.ts
+++ src/__generated__/gql.ts
@@ -2,6 +2,7 @@
 import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
+// @ts-ignore
 const documents = [];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -18,6 +19,7 @@ const documents = [];
 export function graphql(source: string): unknown;
 
 export function graphql(source: string) {
+  // @ts-ignore
   return (documents as any)[source] ?? {};
 }
 
